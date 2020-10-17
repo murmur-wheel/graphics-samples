9b48ac903cbdd374442fec8d491ebe5e6327998b
murmur wheel <murmur.wheel@gmail.com>
2020-10-18 00:22:31 +0800

    fix

--- framework/util/macros.h
+++ framework/util/macros.h
@@ -2,8 +2,8 @@
 // Created by murmur wheel on 2020/10/18.
 //
 
-#ifndef UTIL_MACROS_H
-#define UTIL_MACROS_H
+#ifndef FRAMEWORK_UTIL_MACROS_H
+#define FRAMEWORK_UTIL_MACROS_H
 
 #define DISABLE_COPY_AND_MOVE(NAME)      \
   NAME(const NAME&) = delete;            \
@@ -11,4 +11,4 @@
   NAME& operator=(const NAME&) = delete; \
   NAME&& operator=(NAME&&) = delete;
 
-#endif
+#endif // FRAMEWORK_UTIL_MACROS_H
